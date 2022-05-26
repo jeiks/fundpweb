c2ab2c3a11a616757b178ec27fcc633b5d59cd33
Jacson Rodrigues Correia-Silva <jacsonrcsilva@gmail.com>
2022-05-26 11:39:06 -0300

    Tool to publish the website

---
+++ utils/update-site.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+
+TARGET='docs'
+
+echo 'Building website..'
+bundle exec jekyll build
+echo 'Removing the old one...'
+rm -rf $TARGET
+echo 'Copying the new one...'
+cp -r _site $TARGET
+echo 'Publishing modifications...'
+git commit -am "Updating website"
+git push origin main
